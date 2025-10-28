94e2397220355a441ca0e33a3528d4248fab0da0
Abednego Tenge <abednegotenge180@gmail.com>
2025-10-28 14:33:36 +0000

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,21 @@
+##INNER JOIN
 
+SELECT User.first_name, User.last_name, Booking.*
+FROM User
+INNER JOIN Booking
+ON User.user_id = Booking.user_id;
+
+
+###LEFT JOIN
+
+SELECT Property.property_id, Property.name, Review.rating, Review.comment
+FROM Property
+LEFT JOIN Review
+ON Property.property_id = Review.property_id;
+
+##FULL OUTER JOIN
+
+SELECT User.first_name, User.last_name, Booking.status
+FROM User
+FULL OUTER JOIN Booking
+ON User.user_id = Booking.user_id;
