fd99439bf08a48a9fca01e4c73c17fa45bc5337c
Abednego Tenge <abednegotenge180@gmail.com>
2025-10-28 15:18:30 +0000

    Create aggregations_and_window_functions.sql

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,11 @@
+SELECT u.first_name, u.last_name, COUNT(b.booking_id) AS total_booking
+FROM User u
+JOIN Booking b
+ON u.user_id = b.user_id
+GROUP BY u.user_id, u.first_name, u.last_name;
+
+SELECT p.name, p.location, COUNT(b.booking_id) AS total_booking, RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM Property p
+JOIN Booking b
+ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name, p.location;
